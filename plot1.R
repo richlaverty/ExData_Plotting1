7f003b9917ddcd2f6ccc4ef9f966fbcd784612e2
richlaverty <rich.laverty@gmail.com>
2022-01-05 20:05:44 -0500

    Data is imported, and filtered to the 2 days of interest

--- plot1.R
+++ plot1.R
@@ -14,11 +14,27 @@ if(!file.exists("./data/exdata_data_household_power_consumption.zip"))
   unzip("./data/exdata_data_household_power_consumption.zip", exdir = "./data/")
 }
 
+# Bring the data into R
+
 rawfile <- "household_power_consumption.txt"
+fullData <- read.csv2(paste("./data/",rawfile, sep = ""))
 
-# the file contains more than 2,000,000 rows of data collected at a sampling
-# rate of 1.0 minute, over a period of more than 4 years.  We are actually
-# only interested 2 days in February 2007.
+# fullData is a data frame containing more than 2,000,000 rows
+# of data collected at a sampling rate of 1.0 minute, over a period
+# of more than 4 years.  We are actually only interested 2 days in 
+# February 2007.
 
-startDate <- as.Date("2007-02-01")
-endDate <- as.Date("2007-02-02")
+library(dplyr)
+fullData <- mutate(fullData, Date = as.character(Date))
+fullData <- mutate(fullData, Date = strptime(Date, "%d/%m/%Y"))
+attr(fullData,"tzone") <- "EST"
+
+startDate <- as.POSIXlt("2007-02-01", tz = "EST")
+endDate <- as.POSIXlt("2007-02-02", tz = "EST")
+
+# Now, extract the data between startDate and endDate
+
+twoDays <- filter(fullData, Date >= startDate & Date <= endDate)
+twoDays <- mutate(twoDays, Global_active_power = as.numeric(Global_active_power))
+
+
